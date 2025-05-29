e4761a8fabf67ee671d23a8dffad830f7fd54b86
salah929 <salah.sy@msn.com>
2025-05-30 01:53:20 +0200

    Add basic post view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
-def my_blog(request):
-    return HttpResponse("Hello, Blog!")
+# Create your views here.
+
+
+class PostList(generic.ListView):
+    model = Post
